1560c3da808c4dd552c24d5be7931d92583f7137
jiangyangshen <jiangyangshen@faceunity.com>
2025-11-11 14:41:42 +0800

    modify

--- src/coding/test_algorithm.hpp
+++ src/coding/test_algorithm.hpp
@@ -44,8 +44,34 @@ int test_algorithm_npermute() {
 	return 0;
 }
 
+bool cmp(const int a, const int b) {
+	return a > b;
+}
+
+int test_algorithm_sort() {
+	int a[5] = { 3, 1, 4, 2 };
+	std::vector<int> b = { 3, 1, 4, 2 };
+
+	//sort(a, a + 4);
+	sort(a, a + 4, cmp);
+	for (int i = 0; i < 4; ++i) {
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+
+	sort(b.begin() + 1, b.end());
+	//sort(b.begin() + 1, b.end(), cmp);
+	for (int i = 0; i < b.size(); ++i) {
+		printf("%d ", b[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
+
 
 int test_algorithm() {
 	//return test_algorithm_reverse();
-	return test_algorithm_npermute();
+	//return test_algorithm_npermute();
+	return test_algorithm_sort();
 }
