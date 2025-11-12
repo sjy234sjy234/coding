0f31336800af186730349f6ca1a1bc1265be5581
jiangyangshen <jiangyangshen@foxmail.com>
2025-11-12 19:38:06 +0800

    modify

---
+++ src/t1001/main.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <cstring>
+using namespace std;
+
+
+int main() {
+
+
+  return 0;
+}
